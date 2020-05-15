3ac795c257f31421dbafb0f85704b0fcb3a66cbb
nuriktopskiy <neznayka.2014@yandex.ru>
2020-05-15 13:37:09 +0300

    UPLOAD ALL

---
+++ js/topnav.js
@@ -0,0 +1,13 @@
+
+window.onscroll = function() {myFunction()};
+
+var topnav = document.getElementById("header");
+var sticky = topnav.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    topnav.classList.add("sticky")
+  } else {
+    topnav.classList.remove("sticky");
+  }
+}
